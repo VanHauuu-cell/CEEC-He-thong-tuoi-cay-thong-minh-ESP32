794c3cce20d6ad387c186ff7158872abf553fb2d
Văn Hậu <vanthag444@gmail.com>
2026-04-15 20:27:54 +0700

    Update relay.c

--- main/drivers/relay.c
+++ main/drivers/relay.c
@@ -3,12 +3,17 @@
 
 #define RELAY_PIN 2
 
-void relay_on()
-{
-    gpio_set_level(RELAY_PIN, 1);
+void relay_init(void) {
+    gpio_config_t io_out = {
+        .intr_type = GPIO_INTR_DISABLE, 
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = (1ULL<<RELAY_PIN),
+        .pull_down_en = 0, .pull_up_en = 0
+    };
+    gpio_config(&io_out);
+    gpio_set_level(RELAY_PIN, 0);
 }
 
-void relay_off()
-{
-    gpio_set_level(RELAY_PIN, 0);
+void relay_set(bool state) { 
+    gpio_set_level(RELAY_PIN, state ? 1 : 0); 
 }
